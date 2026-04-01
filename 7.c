95740c38f328969a59b909fe77c8c7470343c5f0
351265312876931 <chadmaximusdecimus@gmail.com>
2026-03-31 22:45:20 -0300

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () { 
+    float raio, area; 
+    scanf("%f", &raio);
+    area = 3.14 * pow(raio,2); 
+    printf("%.2f", area);
+   
+   return 0; }  
+
