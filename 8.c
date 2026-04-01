26cacc5325d09f02916133e028b8c9d35d73fbbe
351265312876931 <chadmaximusdecimus@gmail.com>
2026-03-31 22:48:51 -0300

    Update and rename exerc8.c to 8.c

---
+++ 8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x1, y1; 
+    float x2, y2; 
+    float dist;   
+
+    printf("Ponto A: ");
+    scanf("%f %f", &x1, &y1); 
+
+    printf("Ponto B: ");
+    scanf("%f %f", &x2, &y2); 
+
+    dist = sqrt(pow((x2 - x1),2) + pow((y2 - y1),2));
+
+    printf("Distancia de A e B = %f", dist);
+
+    return 0;
+}
